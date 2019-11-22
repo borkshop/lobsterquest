c0c26da0bba1d8b3aa302025ad7ee9c268260858
Joshua T Corbin <jcorbin@wunjo.org>
2019-11-21 21:58:11 -0800

    moji: further expand sheet gen

--- sheets.go
+++ sheets.go
@@ -12,6 +12,7 @@ import (
 	"image/draw"
 	"image/png"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -22,59 +23,19 @@ import (
 	"unicode"
 )
 
-var sheetsTemplate = template.Must(template.New("sheetsTemplate").Funcs(template.FuncMap{
-	"wrap": func(n int, name, prefix, s string) string {
-		r := []rune(s)
-
-		var sb strings.Builder
-		i := 0
-		seek := func(j int) int {
-			if j > len(r) {
-				return len(r)
-			}
-			k := j
-			for ; j > i; j-- {
-				if unicode.IsSpace(r[j]) {
-					return j
-				}
-			}
-			for ; k < len(r); k++ {
-				if unicode.IsSpace(r[k]) {
-					return k
-				}
-			}
-			return len(r)
+var tmplFuncs = template.FuncMap{
+	"wrap": wrapLines,
+	"special": func(key string) bool {
+		switch key {
+		case "moji", "SpriteID":
+			return true
+		default:
+			return false
 		}
-		skip := func(j int) int {
-			for ; j < len(r); j++ {
-				if !unicode.IsSpace(r[j]) {
-					return j
-				}
-			}
-			return len(r)
-		}
-
-		first := prefix + name + ": "
-		cont := prefix + strings.Repeat(" ", len(name)+2)
-
-		{
-			j := seek(i + n - len(first))
-			_, _ = sb.WriteString(first)
-			_, _ = sb.WriteString(strings.TrimRightFunc(string(r[i:j]), unicode.IsSpace))
-			i = skip(j)
-		}
-
-		for i < len(r) {
-			_ = sb.WriteByte('\n')
-			j := seek(i + n - len(cont))
-			_, _ = sb.WriteString(cont)
-			_, _ = sb.WriteString(strings.TrimRightFunc(string(r[i:j]), unicode.IsSpace))
-			i = skip(j)
-		}
-
-		return sb.String()
 	},
-}).Parse(`
+}
+
+var sheetsTemplate = template.Must(template.New("sheetsTemplate").Funcs(tmplFuncs).Parse(`
 import sprites
 import vec
 
@@ -83,168 +44,304 @@ var sheet_sprites = []::resource
 def load_sheet():
     sheet_sprites = load_sprites("{{ .ImageOut }}", {{ .Sprites.Resolution }}, xy { {{ .Sprites.Size.X }}, {{ .Sprites.Size.Y }} }, {{ .Sprites.Paths | len }})
 
-{{ range .Sheets }}{{ template "sheet" . }}
+{{- range .Sheets }}{{ template "sheet" . }}
 {{ end }}
 
-{{ define "sheet" }}{{ $sheet := . -}}
+{{- define "sheet" }}
+{{- $sheet := . -}}
+{{- $hasMoji := $sheet.HasField "moji" }}
 
 enum {{ $sheet.EntityType }}_entity_id:
     {{ $sheet.EntityType }}_none
-
-{{- range $id, $name := $sheet.Names }}
-{{ range $key := $sheet.InfoKeys $id }}{{ with $sheet.Info $id $key }}
-{{ wrap 80 $key "    // " . }}
-{{- end }}{{ end }}
-    {{ $sheet.Symbol $id }} // {{ $sheet.Moji $id }}
+{{- range $id, $name := $sheet.Name }}
+{{-   $info := index $sheet.Info $id }}
+{{    if $info.Comment }}
+    //// {{ $name }}
+{{-   else }}
+    {{ $sheet.EntityType }}_{{ $name }}{{ if $hasMoji }} // {{ $info.moji }}{{ end }}
+{{-     range $key, $val := $info }}{{ if not (special $key) }}{{ with $val }}
+{{        wrap 80 $key "    // " (print .) }}
+{{-     end }}{{ end }}{{ end }}
 {{-   end }}
+{{- end }}
+
+{{-    if $hasMoji }}
 
 let {{ $sheet.EntityType }}_sprite_id = [
     -1, // {{ $sheet.EntityType }}_none
-{{ range $id, $name := $sheet.Names }}    {{ $sheet.SpriteID $id }}, // {{ $sheet.Symbol $id }}
-{{ end }}]
-{{ end }}
+{{-   range $id, $name := $sheet.Name }}
+{{-     $info := index $sheet.Info $id }}
+{{-     if not $info.Comment }}
+    {{ $info.SpriteID }}, // {{ $sheet.EntityType }}_{{ $name }}
+{{-     end }}
+{{-   end }}
+]
+{{- end }}
+
+{{- end }}
 `))
 
+var (
+	verbose bool
+	mojiDir string
+	sprites Sprites
+)
+
 func main() {
-	var (
-		sourceDir string
-		outBase   string
-		sprites   Sprites
-	)
+	log.SetFlags(0)
+
+	flag.BoolVar(&verbose, "v", false, "enable verbose loggging")
+	flag.StringVar(&mojiDir, "moji-dir", "openmoji", "source glyph directory")
 	flag.IntVar(&sprites.Resolution, "res", 72, "sprite resolution")
-	flag.StringVar(&sourceDir, "dir", "openmoji", "source glyph directory")
-	flag.StringVar(&outBase, "out", "sheets", "output basename")
 	flag.Parse()
 
-	filenames := []string{
-		"sheets/Emoji Quest - Tiles.tsv",
-		"sheets/Emoji Quest - Items.tsv",
-		"sheets/Emoji Quest - Avatars.tsv",
+	baseName := flag.Arg(0)
+	if baseName == "" {
+		progName := os.Args[0]
+		baseName = filepath.Base(progName)
+		baseName = strings.TrimSuffix(baseName, filepath.Ext(baseName))
+		if verbose {
+			log.Printf("INFO: inferred baseName %q from program name %q", baseName, progName)
+		}
 	}
 
-	sheets := make([]Sheet, len(filenames))
-	for i, filename := range filenames {
-		parts := strings.SplitN(filename, " - ", 2)
-		if len(parts) != 2 {
-			log.Fatalf("unrecognized sheet filename: %q", filename)
-		}
-		name := parts[1]
-		name = strings.TrimSuffix(name, filepath.Ext(name))
-		name = strings.TrimSuffix(name, "s")
-		name = strings.ToLower(name)
-		sheets[i] = Sheet{EntityType: name}
+	ents, err := ioutil.ReadDir(baseName)
+	if err != nil {
+		log.Fatalf("failed to read sheets directory: %v", err)
 	}
+	log.Printf("processing %v entries found in %q", len(ents), baseName)
 
-	for i := range sheets {
-		sh := &sheets[i]
-		filename := filenames[i]
-		fmt.Printf("collecting %v moji from %v\n", sh.EntityType, filename)
-		if err := sh.CollectFile(filename); err != nil {
-			log.Fatalf("failed to collect %v entities from %v: %v", sh.EntityType, filename, err)
+	sheets := make([]Sheet, 0, len(ents))
+	for _, ent := range ents {
+		filename := filepath.Join(baseName, ent.Name())
+		if verbose {
+			log.Printf("")
+			log.Printf("INFO: processing %q", filename)
 		}
-		if len(sh.moji) == 0 {
-			log.Printf("no moji found in %v", filename)
+		if sheet, err := loadSheet(filename); isWarning(err) {
+			log.Printf("WARNING: skipping %q: %v", filename, err)
 			continue
+		} else if err != nil {
+			log.Fatalf("FATAL: failed to load %q: %v", filename, err)
+		} else {
+			sheets = append(sheets, sheet)
 		}
-		n := sh.FindGlyphs(&sprites, sourceDir)
+	}
+	sort.Slice(sheets, func(i, j int) bool {
+		return sheets[i].EntityType < sheets[j].EntityType
+	})
+
+	atlasFile := baseName + ".png"
+	{
+		if verbose {
+			log.Printf("")
+		}
+		if err := sprites.BuildFile(atlasFile); err != nil {
+			log.Fatalf("FATAL: failed to assemble sprite atlas: %v", err)
+		}
+		if verbose {
+			log.Printf("INFO: assembled sprite atlas in %q", atlasFile)
+		}
+	}
+
+	codeFile := baseName + ".lobster"
+	{
+		if verbose {
+			log.Printf("")
+		}
+
+		if err := writeTemplateFile(codeFile, sheetsTemplate, struct {
+			ImageOut string
+			Sheets   []Sheet
+			Sprites  Sprites
+		}{atlasFile, sheets, sprites}); err != nil {
+			log.Fatalf("FATAL: failed to compile sheet code: %v", err)
+		}
+		if verbose {
+			log.Printf("INFO: compiled sheet code in %q", codeFile)
+		}
+	}
+}
+
+func loadSheet(filename string) (sheet Sheet, _ error) {
+	if strings.ToLower(filepath.Ext(filename)) != ".tsv" {
+		return sheet, warn("non tsv file")
+	}
+
+	sheetName := parseSheetName(filepath.Base(filename))
+	if sheetName == "" {
+		return sheet, fmt.Errorf("unable to parse sheet name from file name")
+	}
+
+	if err := sheet.ReadFile(filename); err != nil {
+		return sheet, err
+	}
+
+	if sheet.EntityType != sheetName {
+		return sheet, warn("file sheet name and first header cell type name:%q mismatch", sheetName, sheet.EntityType)
+	}
+
+	if verbose {
+		log.Printf("INFO: read %q sheet with %v entries", sheetName, len(sheet.Name))
+	}
+
+	if sheet.HasField("moji") {
+		m := len(sheet.Name)
+		n := sheet.FindGlyphs(&sprites, mojiDir)
 		if n == 0 {
-			log.Fatalf("found no glyphs for %v, missing glyph directory in %q?", filename, sourceDir)
+			return sheet, fmt.Errorf("FATAL: found no %q moji glyphs, missing glyph directory in %q?", sheet.EntityType, mojiDir)
 		}
-		fmt.Printf("Found %v / %v %v moji glyphs\n", n, len(sh.moji), sh.EntityType)
+
+		if verbose {
+			log.Printf("INFO: found %v / %v %q moji glyphs", n, m, sheet.EntityType)
+		}
 	}
 
-	if err := sprites.BuildFile(outBase + ".png"); err != nil {
-		log.Fatalf("failed to assemble sprites: %v", err)
-	}
-
-	if err := writeTemplateFile(outBase+".lobster", sheetsTemplate, struct {
-		ImageOut string
-		Sheets   []Sheet
-		Sprites  Sprites
-	}{outBase + ".png", sheets, sprites}); err != nil {
-		log.Fatalf("failed to write entity code: %v", err)
-	}
+	return sheet, nil
 }
 
 // Sheet represents one data sheet of entity information, with reference into
 // the global sprite table, and associated info.
 type Sheet struct {
 	EntityType string
-	Names      []string
-	flags      [][]string
-	moji       []string
-	info       []map[string]string
-	spriteID   []int
+	Fields     []string
+	Name       []string
+	Info       []map[string]interface{}
 }
 
-func (sheet Sheet) Name(id int) string             { return sheet.Names[id] }
-func (sheet Sheet) Flags(id int) []string          { return sheet.flags[id] }
-func (sheet Sheet) Moji(id int) string             { return sheet.moji[id] }
-func (sheet Sheet) Info(id int, key string) string { return sheet.info[id][key] }
-func (sheet Sheet) SpriteID(id int) int            { return sheet.spriteID[id] }
-func (sheet Sheet) Symbol(id int) string {
-	return fmt.Sprintf("%v_%v", sheet.EntityType, symbolize(sheet.Names[id]))
-}
-func (sheet Sheet) InfoKeys(id int) []string {
-	info := sheet.info[id]
-	keys := make([]string, 0, len(info))
-	for key := range info {
-		keys = append(keys, key)
+func (sheet Sheet) HasField(name string) bool {
+	for _, field := range sheet.Fields {
+		if field == name {
+			return true
+		}
 	}
-	sort.Strings(keys)
-	return keys
+	return false
 }
 
-func (sheet *Sheet) CollectFile(path string) error {
+func (sheet *Sheet) ReadFile(path string) error {
 	file, err := os.Open(path)
 	if err != nil {
 		return err
 	}
-	err = sheet.Collect(file)
+	err = sheet.Read(file)
 	if cerr := file.Close(); err == nil {
 		err = cerr
 	}
 	return err
 }
 
-func (sheet *Sheet) Collect(r io.Reader) error {
-	sc := newTSVScanner(r)
-	for sc.Expect(2) {
+func (sheet *Sheet) Read(r io.Reader) error {
+	const flagsField = "Flags"
 
+	sc := newTSVScanner(r)
+
+	entityType, hasFlags, err := parseEntityType(sc.Header[0])
+	if err != nil {
+		return err
+	}
+	sheet.EntityType = entityType
+
+	fields := sc.Header[1:]
+
+	if hasFlags {
+		sheet.Fields = []string{flagsField}
+	} else {
+		sheet.Fields = []string{}
+	}
+	sheet.Fields = append(sheet.Fields, fields...)
+
+	for sc.Expect(1) {
 		name, _ := sc.Field(0)
-		moji, _ := sc.Field(1)
-		if name == "" || moji == "" {
+
+		if len(sc.Fields) == 1 {
+			sheet.Name = append(sheet.Name, name)
+			sheet.Info = append(sheet.Info, map[string]interface{}{
+				"Comment": true,
+			})
 			continue
 		}
 
-		info := make(map[string]string, len(sc.Fields)-2)
-		for i := 2; i < len(sc.Fields); i++ {
-			if i < len(sc.Header) {
-				key := sc.Header[i]
-				val := sc.Fields[i]
-				info[key] = val
+		var flags []string
+		if hasFlags {
+			nameParts := strings.Split(name, "/")
+			name = nameParts[0]
+			flags = nameParts[:0]
+			for _, part := range nameParts[1:] {
+				if part = symbolize(part); part != "" {
+					flags = append(flags, part)
+				}
 			}
 		}
 
-		// name strings may be of the form "specific/general" as in "car/vehicle"
-		// split the specific from the general categor{y,ies}
-		nameParts := strings.Split(name, "/")
+		name = symbolize(name)
+		if name == "" {
+			continue
+		}
 
-		sheet.Names = append(sheet.Names, nameParts[0])
-		sheet.flags = append(sheet.flags, nameParts[1:])
-		sheet.info = append(sheet.info, info)
-		sheet.moji = append(sheet.moji, moji)
-		sheet.spriteID = append(sheet.spriteID, -1)
+		info := make(map[string]interface{}, 2*len(sc.Fields))
+		if flags != nil {
+			info[flagsField] = flags
+		}
+		for i, field := range fields {
+			if val, defined := sc.Field(i + 1); defined {
+				info[field] = val
+			}
+		}
+
+		sheet.Name = append(sheet.Name, name)
+		sheet.Info = append(sheet.Info, info)
 	}
 	return sc.Err()
 }
 
+func parseSheetName(baseFilename string) string {
+	parts := strings.SplitN(baseFilename, " - ", 2)
+	if len(parts) != 2 {
+		return ""
+	}
+	sheetName := parts[1]
+	sheetName = strings.TrimSuffix(sheetName, filepath.Ext(sheetName))
+	sheetName = strings.TrimSuffix(sheetName, "s")
+	sheetName = symbolize(sheetName)
+	return sheetName
+}
+
+func parseEntityType(entityType string) (_ string, hasFlags bool, _ error) {
+	const flagsSuffix = "/flags..."
+
+	if entityType == "" {
+		return "", false, warn(`missing entity type header; first header cell must contain entity type name`)
+	}
+
+	if hasFlags = strings.HasSuffix(entityType, flagsSuffix); hasFlags {
+		entityType = entityType[:len(entityType)-len(flagsSuffix)]
+	}
+
+	if !strings.HasSuffix(entityType, " name") {
+		return "", false, warn(`first header cell starts with %q, expected ENTITY_TYPE + " name"`, entityType)
+	}
+
+	entityType = entityType[:len(entityType)-4]
+	entityType = symbolize(entityType)
+
+	return entityType, hasFlags, nil
+}
+
 func (sheet *Sheet) FindGlyphs(sprites *Sprites, dir string) (n int) {
-	for id, moji := range sheet.moji {
-		if sheet.spriteID[id] >= 0 {
+	for id := range sheet.Name {
+		info := sheet.Info[id]
+		mojiVal, hasMoji := info["moji"]
+		if !hasMoji {
+			continue
+		}
+		moji, _ := mojiVal.(string)
+		if moji == "" {
+			continue
+		}
+		if _, defined := info["SpriteID"]; defined {
 			n++
 		} else if path := findGlyph(dir, moji); path != "" {
-			sheet.spriteID[id] = sprites.PathSprite(path)
+			info["SpriteID"] = sprites.PathSprite(path)
 			n++
 		}
 	}
@@ -355,6 +452,7 @@ func findGlyph(dir, glyph string) string {
 }
 
 func symbolize(s string) string {
+	s = strings.TrimSpace(s)
 	s = strings.ToLower(s)
 	// TODO coalese any non-alphanumerics?
 	return strings.Join(strings.Split(s, " "), "_")
@@ -369,6 +467,69 @@ func gridSize(n int) image.Point {
 		y++
 	}
 	return image.Pt(x, y)
+}
+
+func wrapLines(n int, name, prefix, s string) string {
+	r := []rune(s)
+
+	var sb strings.Builder
+	i := 0
+	seek := func(j int) int {
+		if j > len(r) {
+			return len(r)
+		}
+		k := j
+		for ; j > i; j-- {
+			if unicode.IsSpace(r[j]) {
+				return j
+			}
+		}
+		for ; k < len(r); k++ {
+			if unicode.IsSpace(r[k]) {
+				return k
+			}
+		}
+		return len(r)
+	}
+	skip := func(j int) int {
+		for ; j < len(r); j++ {
+			if !unicode.IsSpace(r[j]) {
+				return j
+			}
+		}
+		return len(r)
+	}
+
+	first := prefix + name + ": "
+	cont := prefix + strings.Repeat(" ", len(name)+2)
+
+	{
+		j := seek(i + n - len(first))
+		_, _ = sb.WriteString(first)
+		_, _ = sb.WriteString(strings.TrimRightFunc(string(r[i:j]), unicode.IsSpace))
+		i = skip(j)
+	}
+
+	for i < len(r) {
+		_ = sb.WriteByte('\n')
+		j := seek(i + n - len(cont))
+		_, _ = sb.WriteString(cont)
+		_, _ = sb.WriteString(strings.TrimRightFunc(string(r[i:j]), unicode.IsSpace))
+		i = skip(j)
+	}
+
+	return sb.String()
+}
+
+type warning struct{ error }
+
+func isWarning(err error) bool {
+	_, is := err.(warning)
+	return is
+}
+
+func warn(mess string, args ...interface{}) error {
+	return warning{fmt.Errorf(mess, args...)}
 }
 
 func newTSVScanner(r io.Reader) tsvScanner {
@@ -408,6 +569,12 @@ func (sc *tsvScanner) Scan() bool {
 		sc.Fields = nil
 		return false
 	}
-	sc.Fields = strings.Split(sc.Text(), "\t")
+	fields := strings.Split(sc.Text(), "\t")
+
+	for i := len(fields)-1; i >= 0 && fields[i] == ""; i-- {
+		fields = fields[:i]
+	}
+
+	sc.Fields = fields
 	return true
 }
